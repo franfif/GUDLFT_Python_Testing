8bea4860fd9e7cd06eb7d98d3eb93f5bcb327e4d
franfif <francis.barrow@gmail.com>
2023-08-11 16:22:56 -0400

    separated registered and unregistered email tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -8,8 +8,15 @@ def client():
         yield client
 
 
-@pytest.mark.parametrize('email', ['john@simplylift.co', 'admin@irontemple.com', 'wrong.address@test.com'])
+@pytest.mark.parametrize('email', ['john@simplylift.co', 'admin@irontemple.com'])
 def test_login_registered_user(client, email):
     response = client.post('/show_summary', data={'email': email})
     expected_value = 200
+    assert response.status_code == expected_value
+
+
+@pytest.mark.parametrize('email', ['wrong.address@test.com', 'not.an.address'])
+def test_login_unregistered_user(client, email):
+    response = client.post('/show_summary', data={'email': email})
+    expected_value = 302
     assert response.status_code == expected_value
