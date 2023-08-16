bfd8a72dc6b1fa8deba6e7375f1e4857514d9e12
franfif <francis.barrow@gmail.com>
2023-08-16 12:51:05 -0400

    add initial plan for bug fix

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -20,3 +20,17 @@ def test_login_unregistered_user(client, email):
     response = client.post('/show_summary', data={'email': email})
     expected_value = 302
     assert response.status_code == expected_value
+
+
+# test for BUG02
+clubs_for_tests = [{'name': 'Simply Lift', 'email': 'john@simplylift.co', 'points': '13'},
+                   {'name': 'Iron Temple', 'email': 'admin@irontemple.com', 'points': '4'},
+                   {'name': 'She Lifts', 'email': 'kate@shelifts.co.uk', 'points': '12'}]
+
+# mock load_clubs to return the list I want
+# login with an account from mock load_clubs
+# mock book()?
+# test purchase() with trying to purchase more than the number of points the club has.
+# club has 9 points
+# club tries to purchase 10 places
+# function redirects to book(), with an error message "not enough points"
