2499acc1301ec3992bb4de9ec0e062a567b52579
franfif <francis.barrow@gmail.com>
2023-08-17 16:49:18 -0400

    add tests for BUG02

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,5 +1,6 @@
 import pytest
 from server import app
+from utilities.utils import process_purchase
 
 
 @pytest.fixture
@@ -8,18 +9,10 @@ def client():
         yield client
 
 
-@pytest.mark.parametrize('email', ['john@simplylift.co', 'admin@irontemple.com'])
-def test_login_registered_user(client, email):
+@pytest.mark.parametrize('email, expected_status_code', [('john@simplylift.co', 200), ('wrong.address@test.com', 302)])
+def test_login(client, email, expected_status_code):
     response = client.post('/show_summary', data={'email': email})
-    expected_value = 200
-    assert response.status_code == expected_value
-
-
-@pytest.mark.parametrize('email', ['wrong.address@test.com', 'not.an.address'])
-def test_login_unregistered_user(client, email):
-    response = client.post('/show_summary', data={'email': email})
-    expected_value = 302
-    assert response.status_code == expected_value
+    assert response.status_code == expected_status_code
 
 
 # test for BUG02
@@ -27,12 +20,28 @@ clubs_for_tests = [{'name': 'Simply Lift', 'email': 'john@simplylift.co', 'point
                    {'name': 'Iron Temple', 'email': 'admin@irontemple.com', 'points': '4'},
                    {'name': 'She Lifts', 'email': 'kate@shelifts.co.uk', 'points': '12'}]
 
+competitions_for_tests = [{'name': 'Spring Festival', "date": "2020-10-22 13:30:00", "numberOfPlaces": "25"},
+                          {'name': 'Fall Classic', "date": "2020-10-22 13:30:00", "numberOfPlaces": "13"}]
 
-def test_purchase_too_many_places(client):
-    rv = client.post('/purchase_places', data={'competition': "Spring Festival",
-                                               'club': 'Simply Lift',
-                                               'places': 15})
-    data = rv.data.decode()
-    assert rv.status_code == 302
-    assert data.find("Your club does not have enough places.") != -1
 
+@pytest.mark.parametrize('places_required, expected_status_code', [(300, 302), (1, 200)])
+def test_purchase_places(client, places_required, expected_status_code):
+    rv = client.post('/purchase_places', data={'competition': "Fall Classic",
+                                               'club': 'Iron Temple',
+                                               'places': places_required})
+    assert rv.status_code == expected_status_code
+
+
+@pytest.mark.parametrize('club, competition, places_required, processed, messages',
+                         [(clubs_for_tests[0], competitions_for_tests[0], 3, True,
+                           ["Great-booking complete!"]),
+                          (clubs_for_tests[1], competitions_for_tests[1], 400, False,
+                           ["You are not allowed to purchase more than 12 places for a single competition.",
+                            "There are only 13 places left in this competition.",
+                            "You are only able to purchase 4 places for your club."]),
+                          ])
+def test_process_purchase(club, competition, places_required, processed, messages):
+    res_processed, res_messages = process_purchase(club, competition, places_required)
+    assert res_processed is processed
+    for message in messages:
+        assert message in res_messages
