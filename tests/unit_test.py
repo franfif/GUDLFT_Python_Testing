a7d04ec37d6398cc7babe29cadd871e837cc209d
franfif <francis.barrow@gmail.com>
2023-08-22 11:40:58 -0400

    reformat tests into class tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,67 +1,84 @@
 import pytest
 from server import app
 import utilities
-from utilities.utils import process_purchase, competitions
+import server
+from utilities.utils import process_purchase
 
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
+class Client:
+    @staticmethod
+    @pytest.fixture
+    def client():
+        with app.test_client() as client:
+            yield client
 
 
-@pytest.mark.parametrize('email, expected_status_code', [('john@simplylift.co', 200), ('wrong.address@test.com', 302)])
-def test_login(client, email, expected_status_code):
-    response = client.post('/show_summary', data={'email': email})
-    assert response.status_code == expected_status_code
+class TestLogin(Client):
+    @pytest.mark.parametrize('email, expected_status_code', [('john@simplylift.co', 200),
+                                                             ('wrong.address@test.com', 302)])
+    def test_login(self, client, email, expected_status_code):
+        response = client.post('/show_summary', data={'email': email})
+        assert response.status_code == expected_status_code
 
 
-# test for BUG02
-clubs_for_tests = [{'name': 'Simply Lift', 'email': 'john@simplylift.co', 'points': '13'},
-                   {'name': 'Iron Temple', 'email': 'admin@irontemple.com', 'points': '4'},
-                   {'name': 'She Lifts', 'email': 'kate@shelifts.co.uk', 'points': '12'}]
+class DataForTests:
+    @pytest.fixture
+    def setup_data(self, mocker):
+        mocked_clubs = self.clubs_for_tests()
+        mocked_competitions = self.competitions_for_tests()
+        mocked_bookings = self.bookings_for_tests()
+        mocker.patch.object(server, 'competitions', mocked_competitions)
+        mocker.patch.object(server, 'clubs', mocked_clubs)
+        mocker.patch.object(utilities.utils, 'competitions', mocked_competitions)
+        mocker.patch.object(utilities.utils, 'bookings', mocked_bookings)
+        mocker.patch.object(utilities.utils, 'clubs', mocked_clubs)
 
-competitions_for_tests = [{'name': 'Spring Festival', "date": "2020-10-22 13:30:00", "numberOfPlaces": "25"},
-                          {'name': 'Fall Classic', "date": "2020-10-22 13:30:00", "numberOfPlaces": "13"},
-                          {'name': 'Thanksgiving 2023', 'date': '2023-11-23 13:30:00', 'numberOfPlaces': '15'}]
+    @staticmethod
+    def clubs_for_tests():
+        return [{'name': 'Simply Lift', 'email': 'john@simplylift.co', 'points': '13'},
+                {'name': 'Iron Temple', 'email': 'admin@irontemple.com', 'points': '4'},
+                {'name': 'She Lifts', 'email': 'kate@shelifts.co.uk', 'points': '12'}]
 
-booking_for_tests = {'Spring Festival': {'Simply Lift': 0, 'Iron Temple': 0, 'She Lifts': 0},
-                     'Fall Classic': {'Simply Lift': 0, 'Iron Temple': 0, 'She Lifts': 0},
-                     'Thanksgiving 2023': {'Simply Lift': 0, 'Iron Temple': 0, 'She Lifts': 0}}
+    @staticmethod
+    def competitions_for_tests():
+        return [{'name': 'Spring Festival 2023', "date": "2023-04-22 13:30:00", "numberOfPlaces": "25"},
+                {'name': 'Fall Classic 2023', "date": "2023-10-22 13:30:00", "numberOfPlaces": "13"},
+                {'name': 'Thanksgiving 2023', 'date': '2023-11-23 13:30:00', 'numberOfPlaces': '15'}]
 
-
-@pytest.mark.parametrize('places_required, expected_status_code', [(300, 302), (1, 200)])
-def test_purchase_places(client, places_required, expected_status_code):
-    rv = client.post('/purchase_places', data={'competition': "Fall Classic",
-                                               'club': 'Iron Temple',
-                                               'places': places_required})
-    assert rv.status_code == expected_status_code
-
-
-@pytest.mark.parametrize('club, competition, places_required, processed, messages',
-                         [(clubs_for_tests[0], competitions_for_tests[0], 3, True,
-                           ["Great-booking complete!"]),
-                          (clubs_for_tests[1], competitions_for_tests[1], 400, False,
-                           ["You are not allowed to purchase more than 12 places for a single competition.",
-                            "There are only 13 places left in this competition.",
-                            "You are only able to purchase 4 places for your club."]),
-                          ])
-def test_process_purchase(club, competition, places_required, processed, messages):
-    res_processed, res_messages = process_purchase(club, competition, places_required)
-    assert res_processed is processed
-    for message in messages:
-        assert message in res_messages
+    @staticmethod
+    def bookings_for_tests():
+        return {'Spring Festival 2023': {'Simply Lift': 0, 'Iron Temple': 0, 'She Lifts': 0},
+                'Fall Classic 2023': {'Simply Lift': 0, 'Iron Temple': 0, 'She Lifts': 0},
+                'Thanksgiving 2023': {'Simply Lift': 0, 'Iron Temple': 0, 'She Lifts': 0}}
 
 
-def test_process_past_competition(mocker):
-    mocker.patch.object(utilities.utils, 'competitions', competitions_for_tests)
-    mocker.patch.object(utilities.utils, 'bookings', booking_for_tests)
-    res_processed, res_messages = process_purchase(clubs_for_tests[0], competitions_for_tests[1], 4)
-    assert res_processed is False
+class TestPurchase(DataForTests, Client):
+    @pytest.mark.parametrize('places_required, expected_status_code', [(300, 302), (1, 200)])
+    def test_purchase_places(self, client, setup_data, places_required, expected_status_code):
+        rv = client.post('/purchase_places', data={'competition': self.competitions_for_tests()[1]['name'],
+                                                   'club': self.clubs_for_tests()[1]['name'],
+                                                   'places': places_required})
 
+        assert rv.status_code == expected_status_code
 
-def test_process_current_competition(mocker):
-    mocker.patch.object(utilities.utils, 'competitions', competitions_for_tests)
-    mocker.patch.object(utilities.utils, 'bookings', booking_for_tests)
-    res_processed, res_messages = process_purchase(clubs_for_tests[0], competitions_for_tests[2], 4)
-    assert res_processed is True
+    @pytest.mark.parametrize('club, competition, places_required, processed, messages',
+                             [(DataForTests.clubs_for_tests()[1], DataForTests.competitions_for_tests()[1], 400, False,
+                               ["You are not allowed to purchase more than 12 places for a single competition.",
+                                "There are only 13 places left in this competition.",
+                                "You are only able to purchase 4 places for your club."]),
+                              (DataForTests.clubs_for_tests()[0], DataForTests.competitions_for_tests()[1], 3, True,
+                               ["Great-booking complete!"]),
+                              ])
+    def test_process_purchase(self, setup_data, club, competition, places_required, processed, messages):
+        res_processed, res_messages = process_purchase(club, competition, places_required)
+        assert res_processed is processed
+        for message in messages:
+            assert message in res_messages
+
+    @pytest.mark.parametrize('club, competition, places_required, expected_processed',
+                             [(DataForTests.clubs_for_tests()[0], DataForTests.competitions_for_tests()[0], 4, False),
+                              (DataForTests.clubs_for_tests()[0], DataForTests.competitions_for_tests()[2], 4, True),
+                              ])
+    def test_process_current_past_competition(self, setup_data, club, competition, places_required, expected_processed):
+        res_processed, res_messages = process_purchase(club, competition, places_required)
+        assert res_processed is expected_processed
