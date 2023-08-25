7d207496609cbe129596723f0dd185cc1aaa2d73
franfif <francis.barrow@gmail.com>
2023-08-25 10:07:04 -0400

    clean code

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,26 +24,3 @@ def setup_data(mocker):
     yield {'competitions': mocked_competitions,
            'clubs': mocked_clubs,
            'bookings': mocked_bookings}
-
-
-# def clubs_for_tests():
-#     return [{'name': 'Simply Lift', 'email': 'john@simplylift.co', 'points': '13'},
-#             {'name': 'Iron Temple', 'email': 'admin@irontemple.com', 'points': '4'},
-#             {'name': 'She Lifts', 'email': 'kate@shelifts.co.uk', 'points': '12'}]
-#
-#
-# def competitions_for_tests():
-#     return [{'name': 'Spring Festival 2023', "date": "2023-04-22 13:30:00", "numberOfPlaces": "25"},
-#             {'name': 'Fall Classic 2023', "date": "2023-10-22 13:30:00", "numberOfPlaces": "23"},
-#             {'name': 'Thanksgiving 2023', 'date': '2023-11-23 13:30:00', 'numberOfPlaces': '5'}]
-#
-#
-# def bookings_for_tests():
-#     return {'Spring Festival 2023': {'Simply Lift': 0, 'Iron Temple': 0, 'She Lifts': 0},
-#             'Fall Classic 2023': {'Simply Lift': 0, 'Iron Temple': 0, 'She Lifts': 0},
-#             'Thanksgiving 2023': {'Simply Lift': 0, 'Iron Temple': 0, 'She Lifts': 0}}
-#
-#
-# mocked_competitions = competitions_for_tests()
-# mocked_clubs = clubs_for_tests()
-# mocked_bookings = bookings_for_tests()
